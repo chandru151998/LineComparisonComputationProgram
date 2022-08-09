e7e45248130343831b4081a613b0cc85ada9a02d
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-08-09 18:51:18 +0530

    UC2

--- LineModel.cs
+++ LineModel.cs
@@ -15,18 +15,21 @@ namespace UC2
         /// <param name="y1">The y1.</param>
         /// <param name="x2">The x2.</param>
         /// <param name="y2">The y2.</param>
-        public static void LineAndModel()
+        public static void LineAndModel(double x1, double y1, double x2, double y2, double a1, double b1, double a2, double b2)
         {
            double lengthOfLine;
             double lengthOfLine1;
 
-            lengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            lengthOfLine = Math.Sqrt( Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) );
             Console.WriteLine("Length of a given Line is: " + lengthOfLine);
 
             lengthOfLine1 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
             Console.WriteLine("Length of a given Line is: " + lengthOfLine1);
 
-            Console.WriteLine(lengthOfLine.Equals(lengthOfLine1));
+            if (lengthOfLine1 == lengthOfLine)
+            {
+                Console.WriteLine("Both lines are Equal.");
+            }
         }
     }
 }
