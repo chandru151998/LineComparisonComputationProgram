1c95a7c76d56956b88a297813e33ba7471f4966f
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-08-09 18:52:39 +0530

    UC3

--- LineModel.cs
+++ LineModel.cs
@@ -20,17 +20,24 @@ namespace UC3
              double lengthOfLine;
             double lengthOfLine1;
 
-            lengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            lengthOfLine = Math.Sqrt( Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) );
             Console.WriteLine("Length of a given Line is: " + lengthOfLine);
 
             lengthOfLine1 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
             Console.WriteLine("Length of a given Line is: " + lengthOfLine1);
 
-            //0 = lines are equal
-            //1 = second line is smaller
-            //-1 = first line is smaller
-            int result = lengthOfLine.CompareTo(lengthOfLine1);
-            Console.WriteLine("Lines are: " + result);
+            if (lengthOfLine1 == lengthOfLine)
+            {
+                Console.WriteLine("Both lines are Equal.");
+            }
+            else if (lengthOfLine > lengthOfLine1)
+            {
+                Console.WriteLine("Line with co-ordinates x1,y1,x2,y2 is greater and other one os smaller.");
+            }
+            else 
+            {
+                Console.WriteLine("Line with co-ordinates a1,b1,a2,b2 is greater and other one is smaller.");
+            }
         }
     }
 }
