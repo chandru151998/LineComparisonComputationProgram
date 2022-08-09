036cbf5d709ddacce8c2a92be21104ae8d2a7d84
Chandru Kalahalamath <109204252+chandru151998@users.noreply.github.com>
2022-08-09 18:43:46 +0530

    UC3

--- LineModel.cs
+++ LineModel.cs
@@ -8,28 +8,29 @@ namespace UC3
 {
     internal class LineModel
     {
-        public static void LineAndModel()
+        /// <summary>
+        /// Lengthes the of line.
+        /// </summary>
+        /// <param name="x1">The x1.</param>
+        /// <param name="y1">The y1.</param>
+        /// <param name="x2">The x2.</param>
+        /// <param name="y2">The y2.</param>
+        public static void LineAndModel(double x1, double y1, double x2, double y2, double a1, double b1, double a2, double b2)
         {
-            Console.WriteLine("x1 is ");
-            double x1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("x2 is ");
-            double x2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("y1 is ");
-            double y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("y2 is ");
-            double y2 = Convert.ToInt32(Console.ReadLine());
-            if (y1 == y2)
-            {
-                Console.WriteLine("Lines are Equal");
-            }
-            else if (y2 > y1)
-            {
-                Console.WriteLine("Line 2 is greater than line 1");
-            }
-            else
-            {
-                Console.WriteLine("Line 2 is lesser than line 1");
-            }
+             double lengthOfLine;
+            double lengthOfLine1;
+
+            lengthOfLine = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of a given Line is: " + lengthOfLine);
+
+            lengthOfLine1 = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
+            Console.WriteLine("Length of a given Line is: " + lengthOfLine1);
+
+            //0 = lines are equal
+            //1 = second line is smaller
+            //-1 = first line is smaller
+            int result = lengthOfLine.CompareTo(lengthOfLine1);
+            Console.WriteLine("Lines are: " + result);
         }
     }
 }
